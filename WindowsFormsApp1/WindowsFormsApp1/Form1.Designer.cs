156797dd79e8c7bb4c8189ca843f11a0c5de4241
Kosta21111 <constantineluzhbin@yandex.com>
2025-05-03 21:03:11 +0400

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(687, 35);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(44, 16);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "label1";
+            // 
+            // player
+            // 
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(709, 758);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(316, 168);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            this.player.Click += new System.EventHandler(this.player_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.scoreLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.PictureBox player;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int score = 0;
+        private Random random = new Random();
+        private int gravity = 5; // Скорость падения яблок
+        private List<PictureBox> apples = new List<PictureBox>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gameTimer.Start();
+            this.KeyDown += new KeyEventHandler(KeyIsDown);
+            this.DoubleBuffered = true; // Уменьшает мерцание
+        }
+
+        private void SpawnApple()
+        {
+            var apple = new PictureBox
+            {
+                Size = new Size(26, 27),
+                BackColor = Color.Red,
+                Location = new Point(random.Next(0, this.ClientSize.Width - 26), 0),
+                
+            };
+            apple.Image = System.Drawing.Image.FromFile($"Арбуз1.jpg");
+            apples.Add(apple);
+            this.Controls.Add(apple);
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            // Движение корзины влево-вправо
+            if (e.KeyCode == Keys.Left && player.Left > 0)
+                player.Left -= 20;
+            if (e.KeyCode == Keys.Right && player.Right < this.ClientSize.Width)
+                player.Left += 20;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            // Случайное появление яблок
+            if (random.Next(0, 20) == 1)
+                SpawnApple();
+
+            // Движение яблок вниз
+            foreach (var apple in apples.ToList())
+            {
+                apple.Top += gravity;
+
+                // Если яблоко упало за экран — удаляем
+                if (apple.Top > this.ClientSize.Height)
+                {
+                    apples.Remove(apple);
+                    this.Controls.Remove(apple);
+                }
+                // Если игрок поймал яблоко
+                else if (apple.Bounds.IntersectsWith(player.Bounds))
+                {
+                    score++;
+                    scoreLabel.Text = "Счет: " + score;
+                    apples.Remove(apple);
+                    this.Controls.Remove(apple);
+                }
+            }
+        }
+
+        private void player_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
